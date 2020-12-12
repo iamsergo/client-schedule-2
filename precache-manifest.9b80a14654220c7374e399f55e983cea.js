a971e593d94c7931630c23f6dc73e9f3ded379ff
iamsergo <sergio820015@yandex.ru>
2020-12-12 14:22:20 +0300

    Updates

---
+++ precache-manifest.9b80a14654220c7374e399f55e983cea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484d3fad2dc804738ae95a74a6956fb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09ed1000567d94e98511",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "92ea4170e52e89299f29",
+    "url": "./static/css/main.486b4346.chunk.css"
+  },
+  {
+    "revision": "09ed1000567d94e98511",
+    "url": "./static/js/2.f964d58d.chunk.js"
+  },
+  {
+    "revision": "1c9a5c14c094f70c35a5a57a5770d494",
+    "url": "./static/js/2.f964d58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ea4170e52e89299f29",
+    "url": "./static/js/main.e3c832cf.chunk.js"
+  },
+  {
+    "revision": "c1cce86b0dcf52f668b0",
+    "url": "./static/js/runtime-main.cf9ee41a.js"
+  }
+]);
